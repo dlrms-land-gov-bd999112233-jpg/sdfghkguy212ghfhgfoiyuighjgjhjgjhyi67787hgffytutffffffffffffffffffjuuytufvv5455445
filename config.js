0f0a2aea672417d4a8e347f6e940f396bc232538
dlrms-land-gov-bd999112233-jpg <dlrms.land.gov.bd.999112233@gmail.com>
2026-08-25 12:52:17 +0600

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://ltfsopobmmcseekouhd.supabase.co",
+  SUPABASE_URL: "https://ltfsopobmmcseekouhdd.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_IAXZxM_DmmJlhhavUY7uBQ_uQfhWHQQ",
   ADMIN_EMAIL: "dlrms.land.gov.bd.999112233@gmail.com"
 };
